c283be690ad4336da6f459704f0905613e797329
gunner <123@yandex.ru>
2019-11-04 21:43:09 +0300

    add sticky header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,24 +4,20 @@ import { Menu } from "../../Menu/Menu";
 import { Login } from "../../Login/Login";
 
 export const Header = () => {
-  const [isSticky, setIsSticky] = useState<boolean>(false);
+  /* const [isSticky, setIsSticky] = useState<boolean>(false);
 
   const handleScroll = () => {
     let currentScrollPos: number = window.pageYOffset;
-    //console.log("object", currentScrollPos);
-    //console.log("object", prevScrollPos);
-    if (!isSticky || currentScrollPos >= 50) {
-      setIsSticky(!isSticky);
-    } else {
-      setIsSticky(!isSticky);
-    }
+    console.log("d", currentScrollPos);
+    currentScrollPos >= 50 ? setIsSticky(true) : setIsSticky(false);
   };
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-  }, []);
-
+  }, [isSticky]);
+ */
   return (
-    <div className={`Container ${isSticky ? "Header_Sticky" : ""}`}>
+    <div className="Container Header_Sticky">
       <header className="Header Block No-Gutters Text_Center">
         <Menu className="Menu Col2" />
         <Logo className="Logo Col8">
